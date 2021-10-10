9d1b00d86dd24f5fdf5af224af8bd04dfbf716aa
Mikéias Azevedo <contato.mikeiasgabriel@gmail.com>
2021-10-10 16:51:46 -0300

    feat: add base logic

---
+++ GravityGuy/GravityGuy/Actor.h
@@ -0,0 +1,62 @@
+#ifndef _SANCTUARY_ACTOR_H_
+#define _SANCTUARY_ACTOR_H_
+
+#include "Object.h"
+#include "TileSet.h"
+#include "Animation.h"
+#include "Scene.h"
+
+class Actor : public Object
+{
+protected:
+    TileSet* tileSet;
+    Animation* anim;
+    Scene* scene;
+    float speed;
+    int life;
+    int maxLife;
+
+public:
+    Actor(Scene* scene, float speed, int life);
+    ~Actor();
+
+    void Update();
+    void Draw();
+    void OnCollision(Object* obj);
+    void HandleAnimState();
+    TileSet* getTileSet();
+};
+
+inline void Actor::Draw()
+{
+    anim->Draw(x, y, z);
+}
+
+inline Actor::Actor(Scene* scene, float speed, int life) {
+    Actor::scene = scene;
+    Actor::speed = speed;
+    Actor::life = maxLife = life;
+}
+
+inline Actor::~Actor() {
+    delete anim;
+    delete tileSet;
+}
+
+inline void Actor::Update() {
+
+}
+
+inline void Actor::OnCollision(Object* obj) {
+
+}
+
+inline void Actor::HandleAnimState() {
+
+}
+
+inline TileSet* Actor::getTileSet() {
+    return tileSet;
+}
+
+#endif
---
+++ GravityGuy/GravityGuy/Victory.h
@@ -0,0 +1,27 @@
+/***********************************************************************************/
+
+#ifndef _SANCTUARY_VICTORY_H_
+#define _SANCTUARY_VICTORY_H_
+
+// ------------------------------------------------------------------------------
+
+#include "Game.h"
+#include "Sprite.h"
+
+// ------------------------------------------------------------------------------
+
+class Victory : public Game {
+private:
+    Sprite* backg = nullptr;       // pano de fundo
+    bool ctrlKeyESC = false;        // controle do ESC
+
+public:
+    void Init();                    // inicialização
+    void Update();                  // atualização
+    void Draw();                    // desenho
+    void Finalize();                // finalização
+};
+
+// ------------------------------------------------------------------------------
+
+#endif
